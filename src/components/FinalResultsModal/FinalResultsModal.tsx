fffc3dbf09a4dea33496f2d70f6d3e0d046386bf
Sergio <137777587+SergioDiorov@users.noreply.github.com>
2024-06-24 09:32:50 +0300

    feat/add modals (#2)

---
+++ src/components/FinalResultsModal/FinalResultsModal.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+
+import styles from './FinalResultsModal.module.scss';
+import ScoreInfo from 'src/components/ScoreInfo/ScoreInfo';
+import DoneButton from 'src/assets/DoneButton.png';
+
+interface FinalResultsProps {
+  ourTeamScore: number;
+  othersTeamScore: number;
+  open: boolean;
+  handleClose: () => void;
+}
+
+const FinalResultsModal: FC<FinalResultsProps> = ({
+  open,
+  handleClose,
+  ourTeamScore,
+  othersTeamScore,
+}) => {
+  return (
+    <div
+      style={{ display: open ? 'block' : 'none' }}
+      className={styles.finalResultsWrapper}
+    >
+      <div className={styles.finalResultsModal}>
+        <div className={styles.score}>
+          <ScoreInfo title='OUR TEAM' score={ourTeamScore} />
+          <ScoreInfo title='OTHER TEAMS' score={othersTeamScore} />
+        </div>
+        <button onClick={handleClose}>
+          <img src={DoneButton} alt='DoneButton' />
+        </button>
+      </div>
+      <div className={styles.finalResultsBackground}></div>
+    </div>
+  );
+};
+
+export default FinalResultsModal;
